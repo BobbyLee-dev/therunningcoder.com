55153d7d4f4f85966ca9a81007cfec53bb7de684
Bobby Lee <rdlee8181@gmail.com>
2019-11-26 23:13:04 -0800

    Nav Functionality, Hooks, React Spring...

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,17 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import MainMenu from './menu';
 import styled from 'styled-components';
+import { useSpring } from 'react-spring';
 
 const HeaderWrapper = styled.header`
-  background: rgb(26, 29, 39);
+  background: #122738;
   margin-bottom: 1.45rem;
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: row;
   justify-content: space-between;
-  padding: 0 20px;
+  padding: 10px;
   align-items: center;
   position: fixed;
   left: 0;
@@ -18,8 +19,18 @@ const HeaderWrapper = styled.header`
   width: 100%;
   top: 0;
   z-index: 100;
+  @media (min-width: 768px) {
+    padding: 10px 20px;
+  }
   h1 {
+    font-size: 24px;
     margin: 0;
+    @media (min-width: 768px) {
+      font-size: 30px;
+    }
+    @media (min-width: 1400px) {
+      font-size: 40px;
+    }
     a {
       color: rgb(158, 255, 255);
       text-decoration: none;
@@ -27,13 +38,33 @@ const HeaderWrapper = styled.header`
   }
 `;
 
+const mobileNavBreakPoint = `890px`;
+
+const Hamburger = styled.button`
+  color: #ffc600;
+  background: none;
+  z-index: 100;
+  &:hover {
+    cursor: pointer;
+  }
+  @media (min-width: ${mobileNavBreakPoint}) {
+    display: none;
+  }
+`;
+
 const Header = ({ siteTitle }) => {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const navAnimation = useSpring({
+    transform: isNavOpen ? `translate3d(0,0,0)` : `translate3d(100%,0,0)`
+  });
+
   return (
     <HeaderWrapper>
-      <MainMenu />
       <h1>
         <Link to="/">{siteTitle}</Link>
       </h1>
+      <Hamburger onClick={() => setNavOpen(!isNavOpen)}>Menu</Hamburger>
+      <MainMenu style={navAnimation} />
     </HeaderWrapper>
   );
 };
--- src/components/menu.js
+++ src/components/menu.js
@@ -3,26 +3,58 @@ import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import styled from 'styled-components';
 
+const mobileNavBreakPoint = `890px`;
+
+const MainNav = styled(animated.nav)`
+  background: #122738;
+  @media (max-width: 889px) {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+  @media (min-width: ${mobileNavBreakPoint}) {
+    display: block;
+    transform: translate3d(0, 0px, 0px) !important;
+  }
+`;
+
 const MenuListItems = styled.ul`
-  display: flex;
+  /* display: none; */
   list-style: none;
-  justify-content: flex-end;
   margin: 0;
+  @media (min-width: ${mobileNavBreakPoint}) {
+    display: flex;
+
+    justify-content: flex-end;
+  }
   li {
     margin: 0;
     a {
-      color: rgb(255, 198, 0);
+      color: #ffc600;
       padding: 10px 20px;
       text-decoration: none;
     }
     &.has-sub-menu {
       position: relative;
+      a {
+        padding-right: 5px;
+      }
       ul {
-        top: 45px;
+        @media (min-width: 768px) {
+          top: 39px;
+        }
+        @media (min-width: 1400px) {
+          top: 45px;
+        }
         list-style: none;
-        background: red;
+        background: #122738;
         position: absolute;
         margin: 0;
+        border-top: 2px solid rgb(255, 120, 248);
         li {
           a {
             padding: 10px 20px;
@@ -35,7 +67,15 @@ const MenuListItems = styled.ul`
   }
 `;
 
-const MainMenu = ({ siteTitle }) => {
+const SubMenuButton = styled.span`
+  color: #ffc600;
+  margin-right: 15px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const MainMenu = ({ style }) => {
   const mainNav = useStaticQuery(graphql`
     query MainNavQuery {
       wpgraphql {
@@ -64,12 +104,13 @@ const MainMenu = ({ siteTitle }) => {
   const navItemObjects = mainNav.wpgraphql.menus.nodes[0].menuItems.nodes;
 
   const [isToggled, setToggle] = useState(false);
-  const fade = useSpring({
-    display: isToggled ? 'block' : 'none'
+  const reveal = useSpring({
+    display: isToggled ? 'block' : 'none',
+    opacity: isToggled ? '1' : '0'
   });
 
   return (
-    <nav>
+    <MainNav style={style}>
       <MenuListItems>
         {navItemObjects.map(item => {
           return (
@@ -77,16 +118,6 @@ const MainMenu = ({ siteTitle }) => {
               <li
                 className={
                   item.childItems.nodes.length > 0 ? 'has-sub-menu' : ''
-                }
-                onMouseEnter={
-                  item.childItems.nodes.length > 0
-                    ? () => setToggle(!isToggled)
-                    : ''
-                }
-                onMouseLeave={
-                  item.childItems.nodes.length > 0
-                    ? () => setToggle(!isToggled)
-                    : ''
                 }
               >
                 <Link
@@ -104,7 +135,13 @@ const MainMenu = ({ siteTitle }) => {
                   {item.label}
                 </Link>
                 {item.childItems.nodes.length > 0 && (
-                  <animated.ul style={fade}>
+                  <SubMenuButton onClick={() => setToggle(!isToggled)}>
+                    {'</>'}
+                  </SubMenuButton>
+                )}
+
+                {item.childItems.nodes.length > 0 && (
+                  <animated.ul style={reveal}>
                     {item.childItems.nodes.map(childItem => {
                       return (
                         <li key={childItem.id}>
@@ -126,7 +163,7 @@ const MainMenu = ({ siteTitle }) => {
           );
         })}
       </MenuListItems>
-    </nav>
+    </MainNav>
   );
 };
 
