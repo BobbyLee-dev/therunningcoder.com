3c688b89d3367da3da992d218e959c71e52afacc
Bobby Lee <bobby@bobbylee.dev>
2021-12-22 15:02:20 -0800

    feat: testing new branch

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,6 +10,7 @@ const Footer = ({ colorTheme }) => {
     <FooterWrap style={colorTheme.background}>
       <UpperFooter colorTheme={colorTheme} />
       <LowerFooter colorTheme={colorTheme} />
+			<h2>hello</h2>
     </FooterWrap>
   );
 };
